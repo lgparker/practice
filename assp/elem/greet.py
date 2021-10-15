8b4372c372a9b54b81bb9e8c3d990a21aefe1135
Luke Parker <lukegarrisonparker@gmail.com>
2021-10-15 14:39:43 -0500

    implemented a user greeting script in both python and c

---
+++ assp/elem/greet.py
@@ -0,0 +1,12 @@
+""" Elementary 2:
+    Write a program that asks the user for their name
+    and greets them with their name.
+"""
+
+name = input("What is your name?\nEnter name: ")
+if name:
+    print(f"Hello {name}!")
+else:
+    print("Are you a horse with no name?")
+
+
